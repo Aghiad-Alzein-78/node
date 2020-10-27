371307ccd710827a1673477a7ef7401bd38b497f
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2020-10-27 16:24:12 +0300

    creating os_demo file

---
+++ reference/os_demo.js
@@ -0,0 +1,16 @@
+
+const os=require('os')
+
+//get the operating system name
+console.log(os.platform())
+
+//Cpu Architecture
+console.log(os.arch())
+
+//Cpu core info
+console.log(os.cpus())
+
+//Free memory
+console.log(os.freemem())
+//Total memory
+console.log()
