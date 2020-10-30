3baf2a919796b86cfe21c3663f38c2f1aca0d5bf
Aghiad Alzein <aghiad.alzein@gmail.com>
2020-10-30 22:41:06 +0200

    finishing the crash course

--- index.js
+++ index.js
@@ -34,6 +34,47 @@ const server = http.createServer((req, res) => {
     "public",
     req.url === "/" ? "index.html" : req.url
   );
+  //Extension of the File
+  let extname = path.extname(filePath);
+  let contentType = "text/html";
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+  }
+
+  //Reading the convenient file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        res.writeHead(500, { "content-type": "text/html" });
+        res.end(`Server Error:${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "content-type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
